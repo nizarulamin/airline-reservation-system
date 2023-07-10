b885b6442b3d85294ab815d6e1c78077cbbb00ae
Carrot <77528066+nizarulamin@users.noreply.github.com>
2023-07-10 12:14:16 +0800

    login servlet

---
+++ src/main/java/com/AirlineReservationSystem/controller/Login.java
@@ -0,0 +1,65 @@
+package com.AirlineReservationSystem.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.AirlineReservationSystem.beans.User;
+import com.AirlineReservationSystem.model.UserDB;
+
+/**
+ * Servlet implementation class Login
+ */
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Login() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+
+		User user = new User();
+		user.setEmail(email);
+		user.setPassword(password);
+
+		UserDB ud = new UserDB();
+		String s1 = ud.readData(user);
+
+		if (s1.equalsIgnoreCase("success")) {
+			response.sendRedirect("http://localhost:8080/JavaWebApplication/jsp/Welcome.jsp");
+		} else {
+			out.println("Invalid Email or Password");
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- src/main/java/com/AirlineReservationSystem/controller/Register.java
+++ src/main/java/com/AirlineReservationSystem/controller/Register.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.AirlineReservationSystem.beans.User;
+import com.AirlineReservationSystem.model.UserDB;
+
 /**
  * Servlet implementation class Register
  */
@@ -33,17 +36,14 @@ public class Register extends HttpServlet {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-		String name = request.getParameter("fName");
+		// PrintWriter out = response.getWriter();
+		String name = request.getParameter("name");
 		String email = request.getParameter("email");
-		String password = request.getParameter("psw");
+		String password = request.getParameter("password");
 
 		User rb = new User();
 
@@ -55,6 +55,33 @@ public class Register extends HttpServlet {
 		String s1 = ud.insertUser(rb);
 
 		System.out.println(s1);
+		// out.println(name);
+		// out.println(email);
+		// out.println(password);
 	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//			throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		doGet(request, response);
+//		String name = request.getParameter("fName");
+//		String email = request.getParameter("email");
+//		String password = request.getParameter("psw");
+//
+//		User rb = new User();
+//
+//		rb.setName(name);
+//		rb.setEmail(email);
+//		rb.setPassword(password);
+//
+//		UserDB ud = new UserDB();
+//		String s1 = ud.insertUser(rb);
+//
+//		System.out.println(s1);
+//	}
 
 }
